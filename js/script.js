5a336025fbfa352d5596ac7c1f1b8980d02817c5
Fllorins <96663601+Fllorins@users.noreply.github.com>
2023-01-08 00:24:21 +0200

    add file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,48 @@
-const heart = document.querySelector('.heart-img');
-console.log(heart);
-heart.addEventListener('click', function (e) {
-  e.preventDefault();
-  if (this.classList.contains('is-active')) {
-    // this.classList.remove('is-active');
-  } else {
-    this.classList.add('is-active');
-  }
+const hearts = document.querySelector('.propertis__inner');
+if (hearts) {
+  const redHeart = hearts.querySelectorAll('.propertis-card');
+  redHeart.forEach((heart) => {
+    heart.addEventListener('click', (event) => {
+      heart.querySelector('.icon').classList.toggle('icon-active');
+    });
+  });
+}
+
+/* tabs */
+$(document).ready(function () {
+  $('.tab').click(function (e) {
+    e.preventDefault();
+
+    $('.tabs-head .tab').removeClass('active');
+    $('.tabs-content').removeClass('active');
+
+    $(this).addClass('active');
+    $($(this).attr('href')).addClass('active');
+  });
+
+  $('.tab:first').click();
 });
+
+/* radio */
+
+const filter = document.querySelector('.tabs-body');
+
+if (filter) {
+  const items = filter.querySelectorAll('.block-filter');
+
+  items.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      item
+        .querySelector('.block-filter__dropdown')
+        .classList.toggle('_active-filter');
+      item
+        .querySelector('.block-filter__icon')
+        .classList.toggle('_active-filter');
+
+      if (event.target.classList.contains('block-filter__item')) {
+        item.querySelector('.block-filter__value').textContent =
+          event.target.textContent;
+      }
+    });
+  });
+}
---
+++ js/slick.js
@@ -0,0 +1,12 @@
+$('.slider-block').slick({
+  arrows: false,
+  dots: true,
+  adaptiveHeight: true,
+
+  // infinite: false,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  speed: 600,
+  draggable: true,
+  swipe: true,
+});
