ba59c0f4d6179d905455eea0cc7d15f82b2e3277
Fllorins <96663601+Fllorins@users.noreply.github.com>
2023-01-11 15:46:04 +0200

    modifide adapt

--- js/slick.js
+++ js/slick.js
@@ -5,8 +5,32 @@ $('.slider-block').slick({
 
   // infinite: false,
   slidesToShow: 2,
+  responsive: [
+    {
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
   slidesToScroll: 1,
   speed: 600,
   draggable: true,
   swipe: true,
 });
+
+// if ($(window).innerWidth() <= 850) {
+//   $('.propertis__inner').slick({
+//     arrows: false,
+//     dots: true,
+//     adaptiveHeight: true,
+
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     speed: 600,
+//     draggable: true,
+//     swipe: true,
+//   });
+// } else{
+
+// }
